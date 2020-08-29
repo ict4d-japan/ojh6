78778db2af127f019b437a989e7aee8d8d656d65
Shigeki Yamanaka <sy@actit.jp>
2020-08-29 19:54:37 +0900

    firestoreの設定も共通ファイルに変更

--- video-chat/firebase/firestore.js
+++ video-chat/firebase/firestore.js
@@ -5,13 +5,14 @@ import 'firebase/firestore'
 //Vue.use(VueFire)
 import { firestorePlugin } from 'vuefire'
 Vue.use(firestorePlugin)
+import * as config from '../src/config'
 
-var config = {
-  apiKey: '',
-  authDomain: '',
-  databaseURL: '',
-  projectId: '',
-  storageBucket: '',
-  messagingSenderId: ''
+var fs_config = {
+  apiKey: config.FIREBASE_APIKEY,
+  authDomain: config.FIREBASE_DOMAIN + '.firebaseapp.com',
+  databaseURL: 'https://' + config.FIREBASE_DOMAIN + '.firebaseio.com',
+  projectId: config.FIREBASE_DOMAIN ,
+  storageBucket: config.FIREBASE_DOMAIN +'.appspot.com',
+  messagingSenderId: config.FIREBASE_DOMAIN 
 }
-export const db = firebase.initializeApp(config).firestore()
+export const db = firebase.initializeApp(fs_config).firestore()
